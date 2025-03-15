dcca4650a83feecb5dde8fc822ab52510e0cad9b
Aarij khan <aarij0100@gmail.com>
2025-03-15 20:03:34 +0500

    marquee section done

---
+++ src/app/componensts/DiagonalMarquee.jsx
@@ -0,0 +1,70 @@
+"use client";
+
+import { motion, useAnimate } from "framer-motion";
+import { Fragment, useEffect, useRef, useState } from "react";
+
+const DiagonalMarquee = ({deg}) => {
+   console.log("TCL: DiagonalMarquee -> deg", deg)
+   const animation = useRef(null);
+    const [box, setBox] = useState({
+        isHover: false,
+      });
+      const {isHover} = box;
+    const [scope, animate] = useAnimate();
+    const offers = [
+        "Luxury Velvet Matte Lipstick",
+        "Glow Perfection Liquid Foundation",
+        "24K Gold Infused Highlighter",
+        "Silk Touch Hydrating Concealer",
+        "Diamond Shine Lip Gloss",
+          "Silk Touch Hydrating Concealer",
+          "Rose Petal Blush Palette",
+          "Lash Boost Waterproof Mascara",
+          "Magic Fix Setting Spray",
+          "Radiance Illuminating Primer",
+          "Ultra Define Brow Sculptor"
+      ];
+
+      useEffect(() => {
+        if (animation.current) {
+          if (isHover) {
+            animation.current.speed = 0.4;
+          } else {
+            animation.current.speed = 1;
+          }
+        }
+      }, [isHover]);
+      useEffect(() => {
+        animation.current = animate(
+          scope.current,
+          { x: "50%" },
+          { duration: 50, ease: "linear", repeat: Infinity }
+        );
+      }, []);
+  return (
+    <div className="relative w-full h-[50vh] overflow-hidden flex items-center justify-center bg-gray-100">
+      <motion.div
+       ref={scope}
+       onMouseEnter={() => setBox({ ...box, isHover: true })}
+       onMouseLeave={() => setBox({ ...box, isHover: false })}
+        className={`absolute flex space-x-16 h-[20vh] items-center whitespace-nowrap ${deg == 0 ? "rotate-[-5deg]" :"rotate-[6deg]"} rounded-full bg-black`} // Rotate text diagonally
+
+  >
+    {
+      Array.from({length:2}).map((_,idx)=>(
+        <Fragment key={idx}>
+        {offers.map((item, index) => (
+          <span key={index} className="text-4xl text-white font-bold px-8 hover:bg-gradient-to-r from-purple-400 to-pink-400 bg-clip-text hover:text-transparent ">
+            {item}
+          </span>
+        ))}
+        
+        </Fragment>
+
+      ))
+    }
+      </motion.div>
+    </div>
+  );
+};
+export default DiagonalMarquee
---
+++ src/app/componensts/banner.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Banner() {
+  return (
+    <div className='w-full h-[50vh] bg-pink-200'>
+        
+    </div>
+  )
+}
+
+export default Banner
